dd6ccb01528fc106c9a8f936246089ad34b0df9d
Aditya24 <aadityabante@gmail.com>
2020-09-24 14:11:39 +0200

    getting the data via api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "react-particles-js";
 import "./App.css";
+import Clarifai from "clarifai";
+
 const paticlesParam = {
   particles: {
     number: {
@@ -17,16 +20,49 @@ const paticlesParam = {
   },
 };
 
-function App() {
-  return (
-    <div className='App'>
-      <Particles className='particles' params={paticlesParam} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    const app = new Clarifai.App({
+      apiKey: "093514b616d2499e8242d84a0e03000b",
+    });
+
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  render() {
+    return (
+      <div className='App'>
+        <Particles className='particles' params={paticlesParam} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
